8b7a93d4f8f3cb011835ec10141b70f2fa2da3f0
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2025-11-05 00:26:10 +0100

    emaial user

--- app/Notifications/EmailVerificationNotification.php
+++ app/Notifications/EmailVerificationNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\URL;
 
-class EmailVerificationNotification extends Notification
+class EmailVerificationNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
