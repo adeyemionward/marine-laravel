56190a223f77f68f3d804fe1598cb19dbf3e7fdb
seonudoiba <abiodunigbehinadun17@gmail.com>
2025-11-07 12:45:57 +0100

    reset password

--- database/migrations/2025_11_07_111920_create_password_reset_tokens_table.php
+++ database/migrations/2025_11_07_111920_create_password_reset_tokens_table.php
@@ -11,11 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        if (!Schema::hasTable('password_reset_tokens')) {
+            Schema::create('password_reset_tokens', function (Blueprint $table) {
+                $table->string('email')->primary();
+                $table->string('token');
+                $table->timestamp('created_at')->nullable();
+            });
+        } else {
+            Schema::table('password_reset_tokens', function (Blueprint $table) {
+                if (!Schema::hasColumn('password_reset_tokens', 'email')) {
+                    $table->string('email')->primary();
+                }
+                if (!Schema::hasColumn('password_reset_tokens', 'token')) {
+                    $table->string('token');
+                }
+                if (!Schema::hasColumn('password_reset_tokens', 'created_at')) {
+                    $table->timestamp('created_at')->nullable();
+                }
+            });
+        }
     }
 
     /**
